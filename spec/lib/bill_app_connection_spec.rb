56cefe7d8a4a68dcb1fa3aed18043b59341f79a5
Tomas Srna <tomas@srna.sk>
2015-07-22 16:48:19 +0200

    BillApp::Connection tests

--- spec/lib/bill_app_connection_spec.rb
+++ spec/lib/bill_app_connection_spec.rb
@@ -20,4 +20,25 @@ RSpec.describe BillApp::Connection do
     ba = BillApp::Connection.new(attributes_for(:profile))
     expect(ba).to be_truthy
   end
+
+  context 'requests' do
+    let(:profile) { build(:profile) }
+    let(:ba) { BillApp::Connection.new(profile) }
+    context 'invoices' do
+      it 'can be listed' do
+        expect(ba.invoices).to be_an_instance_of(Hash)
+      end
+      it 'can be retrieved' do
+        expect(ba.invoice(123)).to be_an_instance_of(BillApp::Invoice)
+      end
+      it 'has lines' do
+        expect(ba.invoice(353).lines).to be_an_instance_of(Array)
+      end
+    end
+    context 'contacts' do
+      it 'can be retrieved' do
+        expect(ba.contact(567)).to be_an_instance_of(BillApp::Contact)
+      end
+    end
+  end
 end
